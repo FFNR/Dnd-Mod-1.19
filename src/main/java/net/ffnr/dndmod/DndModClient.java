f573e883c3d59762b2289c1ea481997f9e71bc30
jackvadakumchery <jackvadakumchery27@gmail.com>
2023-01-28 17:44:05 -0600

    First Working Key Bind

--- src/main/java/net/ffnr/dndmod/DndModClient.java
+++ src/main/java/net/ffnr/dndmod/DndModClient.java
@@ -1,10 +1,13 @@
 package net.ffnr.dndmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.ffnr.dndmod.event.KeyInputHandler;
 
 public class DndModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
+
+        KeyInputHandler.register();
     }
 }
---
+++ src/main/java/net/ffnr/dndmod/event/KeyInputHandler.java
@@ -0,0 +1,36 @@
+package net.ffnr.dndmod.event;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyInputHandler {
+    public static final String KEY_CATEGORY_DND = "key.category.dndmod.dnd";
+    public static final String KEY_MAIN_UI = "key.dndmod.main_ui";
+
+    public static KeyBinding main_ui;
+
+    public static void registerKeyInputs(){
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (main_ui.wasPressed())
+            {
+                client.player.sendMessage(Text.of("Test"));
+            }
+        });
+    }
+
+    public static void register(){
+        main_ui = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_MAIN_UI,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_U,
+                KEY_CATEGORY_DND
+        ));
+
+        registerKeyInputs();
+    }
+
+}
