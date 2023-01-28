54c8735b8f3afff8d16d4a65f0eed15f5cc4aa91
Jack Vadakumchery <jackvadakumchery@Jacks-MacBook-Pro.local>
2023-01-28 12:06:35 -0600

    Initial commit

---
+++ src/main/java/net/ffnr/dndmod/DndModClient.java
@@ -0,0 +1,10 @@
+package net.ffnr.dndmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DndModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ffnr/dndmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.ffnr.dndmod.mixin;
+
+import net.ffnr.dndmod.DndMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		DndMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
